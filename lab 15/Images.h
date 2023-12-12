b230a3a3e560186fd3e09072380eccbaade72075
Sivby <146477066+Sivby@users.noreply.github.com>
2023-12-12 21:42:51 +0400

    Add files via upload

---
+++ lab 15/Images.h
@@ -0,0 +1,8 @@
+#pragma once
+// Файлы заголовков Windows
+#include <Windows.h>
+void Image0(HDC hdc, int cx, int cy, COLORREF color);
+void Image1(HDC hdc, int cx, int cy, COLORREF color);
+void Image2(HDC hdc, int cx, int cy, COLORREF color);
+void Image3(HDC hdc, int cx, int cy, COLORREF color);
+void Image4(HDC hdc, int cx, int cy, COLORREF color);
